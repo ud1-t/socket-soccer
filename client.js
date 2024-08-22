faff56679a946e5d777c01ec544f78091afe9c2d
ud1-t <udittripathi0101@gmail.com>
2024-08-23 01:16:44 +0530

    connnect-desconnect synchronise

--- client.js
+++ client.js
@@ -1,28 +1,31 @@
 const socket= io('http://localhost:3000')
-const helloButton = document.getElementById('helloButton')
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 let clientBalls = {}
+let selfID
 
 putWallsAround(0, 0, canvas.clientWidth, canvas.clientHeight)
-let startX = 40 + Math.random() * 560
-let startY = 40 + Math.random() * 400
-let playerBall = new Ball(startX, startY, 40, 5)
-playerBall.player = true
-playerBall.maxSpeed = 5
 
-// socket.on('serverToClient', (data) => {
-//     console.log(data)
-// })
-socket.emit('newPlayer', {x: startX, y: startY})
+socket.on('connect', () => {
+    selfID = socket.id
+    let startX = 40 + Math.random() * 560
+    let startY = 40 + Math.random() * 400
+    clientBalls[socket.id] = new Capsule(startX, startY, startX + 80, startY, 40, 5)
+    clientBalls[socket.id].player = true
+    clientBalls[socket.id].maxSpeed = 5
+    userInput(clientBalls[socket.id])
+    socket.emit('newPlayer', {x: startX, y: startY})
+})
+
 socket.on('updatePlayers', (players) => {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
     playerFound = {}
     for(let id in players) {
         if(id !== socket.id && clientBalls[id] === undefined) {
-            clientBalls[id] = new Ball(players[id].x, players[id].y, 40, 5)
+            clientBalls[id] = new Capsule(players[id].x, players[id].y, players[id].x + 80, players[id].y, 40, 5)
+            clientBalls[id].maxSpeed = 5
         } 
         playerFound[id] = true
     }
@@ -36,12 +39,12 @@ socket.on('updatePlayers', (players) => {
     }
 })
 
-function gameLogic() {
-    socket.emit('update', {
-        x: playerBall.pos.x,
-        y: playerBall.pos.y
-    })
-}
+socket.on('positionUpdate', (playerPos) => {
+    for(let i in playerPos) {
+        if(clientBalls[i] === undefined)
+            continue;
+        clientBalls[i].setPosition(playerPos[i].x, playerPos[i].y, playerPos[i].angle)
+    }
+})
 
-userInput(playerBall)
-requestAnimationFrame(mainLoop)
+requestAnimationFrame(renderOnly)
--- userInput.js
+++ userInput.js
@@ -1,19 +1,40 @@
+let justPressed = false;
+
 function userInput(obj){
     canvas.addEventListener('keydown', function(e){
         if(e.keyCode === 37){
+            if(obj.left === false) {
+                justPressed = true;
+            }
             obj.left = true;
         }
         if(e.keyCode === 38){
+            if(obj.up === false) {
+                justPressed = true;
+            }
             obj.up = true;
         }
         if(e.keyCode === 39){
+            if(obj.right === false) {
+                justPressed = true;
+            }
             obj.right = true;
         }
         if(e.keyCode === 40){
+            if(obj.down === false) {
+                justPressed = true;
+            }
             obj.down = true;
         }
         if(e.keyCode === 32){
+            if(obj.action === false) {
+                justPressed = true;
+            }
             obj.action = true;
+        }
+        if(justPressed === true) {
+            emitUserCommands(obj)
+            justPressed = false;
         }
     });
     
@@ -33,5 +54,17 @@ function userInput(obj){
         if(e.keyCode === 32){
             obj.action = false;
         }
+        emitUserCommands(obj)
     });    
+}
+
+function emitUserCommands(obj) {
+    let userCommands = {
+        left: obj.left,
+        up: obj.up,
+        right: obj.right,
+        down: obj.down,
+        action: obj.action
+    }
+    socket.emit('userCommands', userCommands)
 }
